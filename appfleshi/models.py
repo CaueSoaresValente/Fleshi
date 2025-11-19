ef8a9022637c2cc7d6e7e989841eeaa6ed26dd3c
Cauê <cauevalente00@gmail.com>
2025-11-19 16:00:12 -0300

    feat: add class LoginForm and RegisterForm

--- appfleshi/models.py
+++ appfleshi/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from appfleshi import database, login_manager
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
