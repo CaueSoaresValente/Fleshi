32efe1b702865924330179524cda30f5cacd8578
Cauê Valente <cauevalente00@gmail.com>
2025-12-03 16:25:31 -0300

    feat: add feed page and navbar

--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -50,9 +50,15 @@ def profile(user_id):
         return render_template('profile.html', user=current_user, form=photo_form)
     else:
         user = User.query.get(int(user_id))
-        return render_template('profile.html', user=None)
+        return render_template('profile.html', user=user, form=None)
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('homepage'))
+
+@app.route("/feed")
+@login_required
+def feed():
+    photos = Photo.query.order_by(Photo.upload_date.desc()).all()
+    return render_template("feed.html", photos = photos)
